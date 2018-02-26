ab905ca3671a21e376cf1d34020fb5c5ae6063c0
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-02-26 23:00:14 +0200

    refactor

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
-function Data() {
-    this._storage = [];
+function Data(storage) {
+    this._storage = storage;
 }
 Data.prototype.add = function (task) {
     this._storage.push(task);
-}
+};
 Data.prototype.getAll = function () {
     return this._storage;
-}
+};
 Data.prototype.delete = function (taskId) {
     var index = 0;
     var itemIndex = 0;
@@ -20,10 +20,10 @@ Data.prototype.delete = function (taskId) {
     );
 
     this._storage.splice(itemIndex, 1);
-}
+};
 Data.prototype.deleteAll = function () {
     return this._storage = [];
-}
+};
 Data.prototype.marks = function (taskId) {
     var index = 0;
 
@@ -37,4 +37,4 @@ Data.prototype.marks = function (taskId) {
         }
     );
     this._storage[itemIndex].setIsCompleted(true);
-}
+};
--- view.js
+++ view.js
@@ -1,69 +1,59 @@
-var input = document.createElement("input");
-input.type = "text";
-input.className = "input";
-document.body.appendChild(input);
 var mainBlock = document.createElement("div");
+mainBlock.className = "mainBlock";
 
+addButton("Remove all", "removeAll");
+addButton("Add", "add");
+var input = addInput();
 
 var list = document.createElement("ul");
-mainBlock.className = "mainBlock";
 mainBlock.appendChild(list);
+document.body.appendChild(mainBlock);
 
+var controller = new Controller(new View(), new Data([]));
+var eventHandler = new EventHandler(mainBlock, input);
 
-function Menu(element) {
+function EventHandler(rootElement, input) {
     this.add = function () {
         controller.add(input.value);
-    }
+    };
     this.remove = function (taskId) {
         controller.remove(taskId);
-    }
-    this.mark= function (taskId) {
+    };
+    this.mark = function (taskId) {
         controller.mark(taskId);
-    }
+    };
     this.removeAll = function () {
         controller.removeAll();
-    }
+    };
 
     var self = this;
-    element.addEventListener("click", function (event) {
+    rootElement.addEventListener("click", function (event) {
         var target = event.target;
         var action = target.getAttribute("action");
         var taskId = target.getAttribute("taskId");
         if (taskId) {
             self[action](taskId);
-        } else if(action){
+        } else if (action) {
             self[action]();
         }
     });
 }
-new Menu(mainBlock);
 
-
-addNewItemButton();
-addRemoveAllButton();
-
-
-function addRemoveAllButton() {
-    var buttonRemove = document.createElement("button");
-    buttonRemove.innerHTML = "Remove all";
-    buttonRemove.type = "button";
-    buttonRemove.setAttribute("action" , "removeAll");
-    mainBlock.appendChild(buttonRemove);
-}
-
-function addNewItemButton() {
+function addButton(buttonName, action) {
     var buttonAdd = document.createElement("button");
-    buttonAdd.innerHTML = "Add";
+    buttonAdd.innerHTML = buttonName;
     buttonAdd.type = "button";
-    buttonAdd.setAttribute("action" , "add");
+    buttonAdd.setAttribute("action", action);
     mainBlock.appendChild(buttonAdd);
 }
 
-document.body.appendChild(mainBlock);
-
-
-
-var controller = new Controller(new View());
+function addInput() {
+    var input = document.createElement("input");
+    input.type = "text";
+    input.className = "input";
+    document.body.appendChild(input);
+    return input;
+}
 
 function View() {
 
@@ -75,12 +65,12 @@ View.prototype.showTaskList = function (items) {
         var taskName = task.getName();
         var li = document.createElement("li");
         var taskId = task.getId();
-        li.innerHTML = taskName + '<button action="mark" taskId="'  + taskId + '">mark</button>';
-        li.setAttribute("action" , "remove");
-        li.setAttribute("taskId" , task.getId());
-        if(task.getIsCompleted()){
+        li.innerHTML = taskName + '<button action="mark" taskId="' + taskId + '">mark</button>';
+        li.setAttribute("action", "remove");
+        li.setAttribute("taskId", task.getId());
+        if (task.getIsCompleted()) {
             li.style.background = "red";
         }
         list.appendChild(li)
     }
-}
+};
